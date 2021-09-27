c441a18aa4a71df5b54f012c9af36a78704d4537
Gyula Krizsma <gyula.krizsma@softfactors.com>
2021-09-27 12:58:19 +0200

    Persistence layer representation added using the Persistence namespace
    - ChatRepository implementation added
    - StaticDataSource added to represent the database for the moment

---
+++ Sources/Chat.Web/Persistence/ChatRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Chat.Web.Application;
+using Chat.Web.Domain.Models;
+
+namespace Chat.Web.Persistence
+{
+    public class ChatRepository : IChatRepository
+    {
+        public Task<IReadOnlyCollection<MessageItem>> GetMessages()
+        {
+            var staticItems = StaticDataSource.GetMessages();
+
+            return Task.FromResult(staticItems);
+        }
+
+        public Task<MessageItem> AddMessageItem(string user, string message)
+        {
+            var messageItem = StaticDataSource.AddMessageItem(user, message);
+
+            return Task.FromResult(messageItem);
+        }
+    }
+}
---
+++ Sources/Chat.Web/Persistence/StaticDataSource.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Chat.Web.Domain.Models;
+
+namespace Chat.Web.Persistence
+{
+    public static class StaticDataSource
+    {
+        private static List<MessageItem> _messageItems = new[]
+        {
+            new MessageItem("User Server 1", "Hi", DateTime.Now),
+            new MessageItem("User Server 2", "There", DateTime.Now - TimeSpan.FromDays(2)),
+            new MessageItem("User Server 3", "Milord", DateTime.Now - TimeSpan.FromDays(3))
+        }.ToList();
+
+        public static IReadOnlyCollection<MessageItem> GetMessages()
+        {
+            return _messageItems;
+        }
+        
+        public static MessageItem AddMessageItem(string user, string message)
+        {
+            var messageItem = new MessageItem(user, message, DateTime.UtcNow);
+
+            _messageItems.Add(messageItem);
+
+            return messageItem;
+        }
+    }
+}
