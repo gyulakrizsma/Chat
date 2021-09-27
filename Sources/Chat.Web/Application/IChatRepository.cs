731bda2b3df4516285a11217b38836ecd509640c
Gyula Krizsma <gyula.krizsma@softfactors.com>
2021-09-27 12:56:57 +0200

    Application layer representation added using the Application namespace
    - IChatRepository added as a repository interface to query ChatApplication data

---
+++ Sources/Chat.Web/Application/IChatRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Chat.Web.Domain.Models;
+
+namespace Chat.Web.Application
+{
+    public interface IChatRepository
+    {
+        Task<IReadOnlyCollection<MessageItem>> GetMessages();
+        
+        Task<MessageItem> AddMessageItem(string user, string message);
+    }
+}
