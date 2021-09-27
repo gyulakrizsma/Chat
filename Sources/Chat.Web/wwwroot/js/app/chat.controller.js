c323f09d9d841a9c50717304ab5b419428bfbe77
Gyula Krizsma <gyula.krizsma@softfactors.com>
2021-09-27 12:29:44 +0200

    Chat basic/undesigned view and its controller have been added

---
+++ Sources/Chat.Web/wwwroot/js/app/chat.controller.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('ChatApp')
+        .controller('ChatController', ChatController);
+
+    ChatController.$inject = ['ChatSignalrService', '$scope'];
+
+    function ChatController(chatSignalrService, $scope) {
+        var vm = this;
+
+        vm.$onInit = init;
+
+        vm.user = 'User 1';
+        vm.message = 'message 1';
+        vm.messages = [];
+
+        vm.send = sendMessage;
+
+        function init() {
+            chatSignalrService.startConnection();
+            chatSignalrService.onMessageReceived(handleMessageReceived);
+        }
+        
+        function sendMessage() {
+            chatSignalrService.sendMessage(vm.user, vm.message);
+        }
+
+        function handleMessageReceived(user, message) {
+            vm.messages.push({ user: user, message: message });
+            $scope.$apply();
+        }
+    };
+})();
