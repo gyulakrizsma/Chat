b0cd08ad0fcff646ca6b418b5a2bcc1bc79efb85
Gyula Krizsma <gyula.krizsma@softfactors.com>
2021-09-27 12:29:14 +0200

    Chat angular module and signalr services have been added

---
+++ Sources/Chat.Web/wwwroot/js/app/chat.signalr.services.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('ChatApp')
+        .service('ChatSignalrService', ChatSignalrService);
+
+    function ChatSignalrService() {
+
+        var connection;
+
+        var services = {
+            startConnection: startConnection,
+            sendMessage: sendMessage,
+            onMessageReceived: onMessageReceived
+        };
+
+        function startConnection() {
+            connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+            connection.start();
+        }
+
+        function sendMessage(user, message) {
+            return connection.invoke("SendMessageAsync", user, message);
+        }
+
+        function onMessageReceived(callback) {
+            connection.on("ReceiveMessage",
+                function (user, message) {
+                    return callback(user, message);
+                });
+        }
+
+        return services;
+    };
+})();
