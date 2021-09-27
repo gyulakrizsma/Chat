ef7c8359b51ffbf0ff4a86e14e832dd303fc0712
Gyula Krizsma <gyula.krizsma@softfactors.com>
2021-09-27 12:54:53 +0200

    MessageItem domain model added to the application

---
+++ Sources/Chat.Web/Domain/Models/MessageItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Chat.Web.Domain.Models
+{
+    public class MessageItem
+    {
+        public MessageItem(string user, string message, DateTime createdAt)
+        {
+            User = user;
+            Message = message;
+            CreatedAt = createdAt;
+        }
+
+        public string User { get; }
+
+        public string Message { get; }
+
+        public DateTime CreatedAt { get; }
+    }
+}
