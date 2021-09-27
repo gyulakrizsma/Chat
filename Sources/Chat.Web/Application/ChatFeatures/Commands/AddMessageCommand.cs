17aa7a0534bc4c483700bcd86e4f04405752191d
Gyula Krizsma <gyula.krizsma@softfactors.com>
2021-09-27 13:14:48 +0200

    AddMessageCommand and AddMessageCommand has been implemented to support the Create message item use case

---
+++ Sources/Chat.Web/Application/ChatFeatures/Commands/AddMessageCommand.cs
@@ -0,0 +1,7 @@
+﻿using Chat.Web.Domain.Models;
+using MediatR;
+
+namespace Chat.Web.Application.ChatFeatures.Commands
+{
+    public record AddMessageCommand(string User, string Message) : IRequest<MessageItem>;
+}
---
+++ Sources/Chat.Web/Application/ChatFeatures/Commands/Handlers/AddMessageHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Chat.Web.Domain.Models;
+using MediatR;
+
+namespace Chat.Web.Application.ChatFeatures.Commands.Handlers
+{
+    public class AddMessageHandler : IRequestHandler<AddMessageCommand, MessageItem>
+    {
+        private readonly IChatRepository _repository;
+
+        public AddMessageHandler(IChatRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<MessageItem> Handle(AddMessageCommand request, CancellationToken cancellationToken)
+        {
+            return await _repository.AddMessageItem(request.User, request.Message);
+        }
+    }
+}
