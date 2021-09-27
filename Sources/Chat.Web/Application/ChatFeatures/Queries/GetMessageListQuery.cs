f955e85b35d93470db8ef21d5f7ccb4e57339c7a
Gyula Krizsma <gyula.krizsma@softfactors.com>
2021-09-27 13:14:24 +0200

    GetMessageListQuery and GetMessageListHandler implemented to segregate the get message item use case

---
+++ Sources/Chat.Web/Application/ChatFeatures/Queries/GetMessageListQuery.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using Chat.Web.Domain.Models;
+using MediatR;
+
+namespace Chat.Web.Application.ChatFeatures.Queries
+{
+    public record GetMessageListQuery : IRequest<IReadOnlyList<MessageItem>>;
+}
---
+++ Sources/Chat.Web/Application/ChatFeatures/Queries/Handlers/GetMessageListHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Chat.Web.Domain.Models;
+using MediatR;
+
+namespace Chat.Web.Application.ChatFeatures.Queries.Handlers
+{
+    public class GetMessageListHandler : IRequestHandler<GetMessageListQuery, IReadOnlyList<MessageItem>>
+    {
+        private readonly IChatRepository _repository;
+
+        public GetMessageListHandler(IChatRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IReadOnlyList<MessageItem>> Handle(GetMessageListQuery request, CancellationToken cancellationToken)
+        {
+            return (await _repository.GetMessages()).OrderBy(mi => mi.CreatedAt).ToList();
+        }
+    }
+}
